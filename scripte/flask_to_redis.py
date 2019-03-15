19fbeb158070454f72a480ccb8b9be20ef61ff3c
xiexinhai <xinhaix@opera.com>
2019-03-15 18:27:49 +0800

    update

--- scripte/flask_to_redis.py
+++ scripte/flask_to_redis.py
@@ -14,16 +14,20 @@ redis_port = sys.argv[2]
 pool = redis.ConnectionPool(host=redis_host, port=redis_port)
 redis_cli = redis.StrictRedis(connection_pool=pool)
 
+COUNTRY = "country"
+LANGUAGE = "language"
+TRENDING_IDS = "trending_ids"
+
 
 def validate(data):
     try:
         data_dic = json.loads(data)
-        country = data_dic.get("country", "")
-        language = data_dic.get("language", "")
+        country = data_dic.get(COUNTRY, "")
+        language = data_dic.get(LANGUAGE, "")
         if not country or not language:
             app.logger.warning("validate data failed country=%s language=%s" % (country, language))
             return False
-        if not data_dic("trending_info", ""):
+        if not data_dic(TRENDING_IDS, ""):
             app.logger.warning("validate data failed trending data is Empty")
             return False
     except Exception, e:
@@ -37,7 +41,8 @@ def to_redis():
     if not validate(data):
         return make_response(json.dumps({"status": 400, "reason": "data invalidate"}), 200)
 
-    response = redis_cli.set("name", value=data)
+    data = json.loads(data)
+    response = redis_cli.set("external_hot_topic_mapping_%s_%s" % (data.get(COUNTRY), data.get(LANGUAGE)), value=json.dumps(json.get(TRENDING_IDS)))
     app.logger.info("====== %s" % response)
     return make_response(json.dumps({"status": 200}), 200)
 
@@ -46,3 +51,6 @@ if __name__ != "__main__":
     gunicore_logger = logging.getLogger("gunicore.error")
     app.logger.handlers = gunicore_logger.handlers
     app.logger.setLevel(gunicore_logger.level)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000', debug=True)
